66244fc180434ef017bf20af24e2c51649ae7687
Joaquin <joaquin.melone.zarauza@gmail.com>
2023-01-15 18:40:00 -0300

    Update script.js
    -Added the book constructor a class

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ const submitButton = document.getElementById("submit-button");
 
 // Book constructor
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.read = read;
+    }
 }
 
 function addBookToLibrary() {
